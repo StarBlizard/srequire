16665b5abe295b091e7a26ee4780280d572ff5d9
Yeltzin Salvador <blizzard.yeltzin@gmail.com>
2020-03-04 19:40:27 -0600

    add: setBaseRoutes feature

--- index.js
+++ index.js
@@ -1,4 +1,56 @@
 const cwd      = process.cwd();
 const { join } = require('path');
+const _        = require('underscore');
 
-module.exports = pathFromCWD => require(join(cwd, pathFromCWD));
+/*
+ * dir can be:
+ * 'path/to/something'
+ *
+ * or...
+ *
+ * 'underscore'
+ * */
+
+let savedRoutes = {};
+
+module.exports = dir => {
+  if (!dir)                     { throw new Error('Please specify a route');  }
+  if ((typeof dir) != 'string') { throw new Error('srequire needs a string'); }
+
+  if (dir.indexOf('/') == -1) { return require(dir); }
+
+  const routeName  = dir.split('/')[0];
+  const savedRoute = savedRoutes[routeName]
+
+  if (!savedRoute) { return join(route, dir); }
+
+  const route = savedRoute ? join(cwd, dir.replace(routeName, savedRoute)) : cwd;
+
+  return require(route);
+};
+
+/*
+ *  routes: {
+ *    models: 'path/to/models',
+ *    schemas: 'path/to/schemas',
+ *    anything: 'path/to/any/anything'
+ *  }
+ *
+ *  then...
+ *
+ *  srequire('models/modelX')
+ *  srequire('schemas/users')
+ *  srequire('anythingl/athing')
+ * */
+
+module.exports.setBaseRoutes = routes => {
+  savedRoutes = _.extend(savedRoutes, routes);
+  return savedRoutes;
+};
+
+module.exports.setBaseRoute = (name, route) => {
+  savedRoutes[name] = route;
+  return savedRoutes;
+};
+
+module.exports.routes = (name, route) => savedRoutes;
